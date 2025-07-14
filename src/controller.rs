3980948c152d170ae17954e03362b0f293c39b06
areif-dev <aj@ajreifsnyder.com>
2025-07-14 00:10:35 -0400

    Add login and product_from_ean to required functions

--- src/controller.rs
+++ src/controller.rs
@@ -3,9 +3,10 @@ use ean13::Ean13;
 use crate::Product;
 
 pub trait VendorController {
-    fn search<S>(&self, query: S) -> Vec<Product>
-    where
-        S: ToString;
+    fn login(&self) -> impl std::future::Future<Output = Result<(), fantoccini::error::CmdError>>;
 
-    fn fetch_details(&self, ean: Ean13);
+    fn product_from_ean(
+        &self,
+        ean: Ean13,
+    ) -> impl std::future::Future<Output = Result<Product, fantoccini::error::CmdError>>;
 }
