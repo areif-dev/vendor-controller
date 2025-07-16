32b422b931e40d431a13e3c55a4799c558dd5e23
areif-dev <aj@ajreifsnyder.com>
2025-07-16 17:01:44 -0400

    Supress warnings by removing async fn in trait

--- src/controller.rs
+++ src/controller.rs
@@ -30,38 +30,45 @@ pub trait VendorController {
     /// # Errors
     ///
     /// Forwards any [`fantoccini::error::CmdError`] that arises during the login attempt
-    async fn login(&self) -> Result<(), fantoccini::error::CmdError> {
-        self.client().client.goto(&Self::base_url()).await?;
-        let login_form_elem = self.client().client.find(Locator::Css("form")).await?;
-        let inputs = login_form_elem.find_all(Locator::Css("input")).await?;
-        let (mut user_input, mut passwd_input) = (None, None);
-        for input in inputs {
-            if user_input.is_some() && passwd_input.is_some() {
-                break;
+    fn login(
+        &self,
+    ) -> impl std::future::Future<Output = Result<(), fantoccini::error::CmdError>> + Send
+    where
+        Self: Sync,
+    {
+        async {
+            self.client().client.goto(&Self::base_url()).await?;
+            let login_form_elem = self.client().client.find(Locator::Css("form")).await?;
+            let inputs = login_form_elem.find_all(Locator::Css("input")).await?;
+            let (mut user_input, mut passwd_input) = (None, None);
+            for input in inputs {
+                if user_input.is_some() && passwd_input.is_some() {
+                    break;
+                }
+                let name = input
+                    .prop("name")
+                    .await?
+                    .unwrap_or(String::new())
+                    .to_lowercase();
+                if name.contains("user") {
+                    user_input = Some(input);
+                } else if name.contains("pass") {
+                    passwd_input = Some(input);
+                }
             }
-            let name = input
-                .prop("name")
-                .await?
-                .unwrap_or(String::new())
-                .to_lowercase();
-            if name.contains("user") {
-                user_input = Some(input);
-            } else if name.contains("pass") {
-                passwd_input = Some(input);
-            }
+            let user_input = user_input.ok_or(fantoccini::error::CmdError::InvalidArgument(
+                String::from("user_input"),
+                String::from("Could not find a valid username input field on the page"),
+            ))?;
+            let passwd_input = passwd_input.ok_or(fantoccini::error::CmdError::InvalidArgument(
+                String::from("passwd_input"),
+                String::from("Could not find a valid password input field on the page"),
+            ))?;
+            user_input.send_keys(&self.get_user()).await?;
+            passwd_input.send_keys(&self.get_passwd()).await?;
+            passwd_input.send_keys("\n").await?;
+            Ok(())
         }
-        let user_input = user_input.ok_or(fantoccini::error::CmdError::InvalidArgument(
-            String::from("user_input"),
-            String::from("Could not find a valid username input field on the page"),
-        ))?;
-        let passwd_input = passwd_input.ok_or(fantoccini::error::CmdError::InvalidArgument(
-            String::from("passwd_input"),
-            String::from("Could not find a valid password input field on the page"),
-        ))?;
-        user_input.send_keys(&self.get_user()).await?;
-        passwd_input.send_keys(&self.get_passwd()).await?;
-        passwd_input.send_keys("\n").await?;
-        Ok(())
     }
 
     /// Navigate the vendor's online catalog to fetch product information for a particular [`Ean13`] or UPC.
@@ -98,20 +105,28 @@ pub trait VendorController {
     /// # Errors
     ///
     /// Forwards any [`fantoccini::error::CmdError`]s that arise
-    async fn price_from_elem<'a>(&self, search: Locator<'a>) -> Result<Option<Decimal>, CmdError> {
-        let price_elem = self.client().client.find(search).await?;
-        let mut price_str: String = price_elem.prop("value").await?.unwrap_or(String::new());
-        if price_str == String::new() {
-            price_str = price_elem.text().await.unwrap_or(String::new());
+    fn price_from_elem<'a>(
+        &self,
+        search: Locator<'a>,
+    ) -> impl std::future::Future<Output = Result<Option<Decimal>, CmdError>> + Send
+    where
+        Self: Sync,
+    {
+        async move {
+            let price_elem = self.client().client.find(search).await?;
+            let mut price_str: String = price_elem.prop("value").await?.unwrap_or(String::new());
+            if price_str == String::new() {
+                price_str = price_elem.text().await.unwrap_or(String::new());
+            }
+            price_str = price_str
+                .chars()
+                .filter(|c| c.is_digit(10) || *c == '.')
+                .collect();
+            println!("{}", price_str);
+            let Ok(val) = Decimal::from_str_exact(&price_str) else {
+                return Ok(None);
+            };
+            Ok(Some(val))
         }
-        price_str = price_str
-            .chars()
-            .filter(|c| c.is_digit(10) || *c == '.')
-            .collect();
-        println!("{}", price_str);
-        let Ok(val) = Decimal::from_str_exact(&price_str) else {
-            return Ok(None);
-        };
-        Ok(Some(val))
     }
 }
